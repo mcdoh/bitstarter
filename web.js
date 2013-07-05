8d5d9e07e74a7281e9a7eebd3daedb78cca4fafd
Gavin McDonald <gavinmcdoh@gmail.com>
2013-07-05 16:10:20 +0000

    printing with buffer instead of returning file

--- web.js
+++ web.js
@@ -3,15 +3,14 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 var indexHTML = fs.readFileSync('index.html');
-// var buf = new Buffer(indexHTML);
+var buf = new Buffer(indexHTML);
 
 app.get('/', function(request, response) {
-//	response.send(buf.toString());
-	response.send(indexHTML);
+	response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
 
