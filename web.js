be31a26c2bdc0a7d00cf8dfbc0ccea0b85796033
Gavin McDonald <gavinmcdoh@gmail.com>
2013-07-05 16:08:25 +0000

    reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var indexHTML = fs.readFileSync('index.html');
+// var buf = new Buffer(indexHTML);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//	response.send(buf.toString());
+	response.send(indexHTML);
 });
 
 var port = process.env.PORT || 5000;
